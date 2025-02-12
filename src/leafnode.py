394ea86a2dbe17227e234d55fc97f1c5a6b88b19
venidachev <venidachev@gmail.com>
2025-02-12 15:09:54 +0200

    leaf nodes and unit tests

---
+++ src/leafnode.py
@@ -0,0 +1,13 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        if value is None:
+            raise ValueError
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
---
+++ src/test_leafnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_paragraph(self):
+        leaf = LeafNode("p", "This is a paragraph of text.")
+        html = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(leaf.to_html(), html)
+
+    def test_props(self):
+        leaf = LeafNode("a", "Link!", {
+            "href": "https://www.google.com",
+            "target": "_blank"
+        })
+        html = '<a href="https://www.google.com" target="_blank">Link!</a>'
+        self.assertEqual(leaf.to_html(), html)
+
+    def test_no_tag(self):
+        leaf = LeafNode(None, "Some", {"href": "localhost:8888"})
+        html = "Some"
+        self.assertEqual(leaf.to_html(), html)
+
+    def test_no_value(self):
+        with self.assertRaises(ValueError):
+            leaf = LeafNode("p", None)
