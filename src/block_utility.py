182a6f8751f490d2a3425009a0bd4a4167667fa0
venidachev <venidachev@gmail.com>
2025-02-14 16:08:34 +0200

    Add block to block type function

--- src/block_utility.py
+++ src/block_utility.py
@@ -1,3 +1,27 @@
+import re
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     """Takes a raw Markdown string (representing a full document) as input and returns a list of \"block\" strings."""
     return [block.strip() for block in markdown.split("\n\n") if block.strip()]
+
+def block_to_block_type(block: str) -> str:
+    """Takes a single block of markdown text as input and returns a string representing the type of block it is.
+    The 6 types being: paragraph, heading, code, quote, unordered_list, ordered_list."""
+    # Heading
+    regex = r"^#{1,6} "
+    if re.match(regex, block):
+        return "heading"
+    # Code
+    if block.startswith("```") and block.endswith("```"):
+        return "code"
+    # Quote
+    if all(line.startswith(">") for line in block.split("\n")):
+           return "quote"
+    # Unordered list
+    if all(line.startswith("* ") or line.startswith("- ") for line in block.split("\n")):
+         return "unordered_list"
+    # Ordered list
+    if all(line.startswith(f"{i}. ") for i, line in enumerate(block.split("\n"), start=1)):
+        return "ordered_list"
+    # Paragraph
+    return "paragraph"
--- src/test_block_utility.py
+++ src/test_block_utility.py
@@ -1,5 +1,5 @@
 import unittest
-from block_utility import markdown_to_blocks
+from block_utility import markdown_to_blocks, block_to_block_type
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_basic_functionality(self):
@@ -62,4 +62,131 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         markdown = "This is a heading\nThis is a paragraph"
         expected = ["This is a heading\nThis is a paragraph"]
         output = markdown_to_blocks(markdown)
+        self.assertEqual(expected, output)
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_paragraph(self):
+        block = "This is a paragraph"
+        expected = "paragraph"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_heading(self):
+        block = "# This is a heading"
+        expected = "heading"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_heading_no_space(self):
+        block = "#This is a heading"
+        expected = "paragraph"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_heading_6_hashes(self):
+        block = "###### This is a heading"
+        expected = "heading"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_heading_7_hashes(self):
+        block = "####### This is a heading"
+        expected = "paragraph"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_unordered_list_asterisk(self):
+        block = """* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+        expected = "unordered_list"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_unordered_list_dash(self):
+        block = """- This is the first list item in a list block
+- This is a list item
+- This is another list item"""
+        expected = "unordered_list"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_unordered_list_one_invalid(self):
+        block = """- This is the first list item in a list block
+ This is a list item
+- This is another list item"""
+        expected = "paragraph"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_unordered_list_mixed(self):
+        block = """- This is the first list item in a list block
+- This is a list item
+* This is another list item"""
+        expected = "unordered_list"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_code(self):
+        block = """```- This is the first list item in a list block
+- This is a list item
+- This is another list item```"""
+        expected = "code"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_code_unclosed(self):
+        block = """```- This is the first list item in a list block
+- This is a list item
+- This is another list item``"""
+        expected = "paragraph"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_ordered_list(self):
+        block = """1. This is the first list item in a list block
+2. This is a list item
+3. This is another list item``"""
+        expected = "ordered_list"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+    
+    def test_ordered_list_no_dot(self):
+        block = """1. This is the first list item in a list block
+2 This is a list item
+3. This is another list item``"""
+        expected = "paragraph"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_ordered_list_invalid_start(self):
+        block = """2. This is the first list item in a list block
+3. This is a list item
+4. This is another list item``"""
+        expected = "paragraph"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_ordered_list_no_space(self):
+        block = """1.This is the first list item in a list block
+2. This is a list item
+3. This is another list item``"""
+        expected = "paragraph"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_quote(self):
+        block = """>This is the first list item in a list block
+> This is a list item
+>> This is another list item``"""
+        expected = "quote"
+        output = block_to_block_type(block)
+        self.assertEqual(expected, output)
+
+    def test_quote_missed_line(self):
+        block = """>This is the first list item in a list block
+> This is a list item
+This is another list item``"""
+        expected = "paragraph"
+        output = block_to_block_type(block)
         self.assertEqual(expected, output)
